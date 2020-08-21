d092c9c75c24c63315c8fff237e70dc22ef57dae
Nicolas Earnshaw <nearnshaw@decentraland.org>
2020-08-21 16:34:55 -0300

    added isAuthorizedAndHasBalance to marketplace index

--- marketplace/index.ts
+++ marketplace/index.ts
@@ -1,5 +1,6 @@
 export { getContract } from "./contract"
 
+export { isAuthorizedAndHasBalance } from './functions/isAuthorizedAndHasBalance'
 export { isAuthorizedAll } from './functions/isAuthorizedAll'
 export { cancelOrder } from './functions/cancelOrder'
 export { createOrder } from './functions/createOrder'
