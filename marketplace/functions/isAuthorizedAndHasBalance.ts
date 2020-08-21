3fe87b9831532e454d0eca74f61a57447e8047aa
Kyllian LE BORGNE ROEPRCH <me@hprivakos.net>
2020-08-22 01:57:16 +0200

    finish isAuthorizedAndHasBalance()

--- marketplace/functions/isAuthorizedAndHasBalance.ts
+++ marketplace/functions/isAuthorizedAndHasBalance.ts
@@ -1,15 +1,15 @@
 import { getUserAccount } from '@decentraland/EthereumController'
 import { allowance } from '../../currency/index'
 import { mainnet as Addresses } from '../../utils/contract'
+import { toWei } from 'eth-connect'
+import { myBalance } from 'mana'
 
-export async function isAuthorizedAndHasBalance(
-  price: number,
-  userAddress?: string
-) {
+export async function isAuthorizedAndHasBalance(price: string, userAddress?: string) {
   if (!userAddress) userAddress = await getUserAccount()
-  const authorized = await allowance(
-    Addresses.MANAToken,
-    userAddress,
-    Addresses.MarketplaceProxy
-  )
+  const authorized = await allowance(Addresses.MANAToken, userAddress, Addresses.MarketplaceProxy)
+  if (+authorized < +toWei(price, 'ether').toString()) return false
+
+  const balance = await myBalance()
+  if (+balance >= +toWei(price, 'ether').toString()) return true
+  else return false
 }
