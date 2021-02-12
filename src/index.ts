2460b78990df3b371085ae6cc3f313425b3db860
Kyllian LE BORGNE ROEPRCH <me@hprivakos.net>
2021-02-12 22:52:05 +0100

    fix: add contract to index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import * as avatar from './avatar/index'
-import * as ethereum from './ethereum/index'
-import * as mana from './mana/index'
-import * as currency from './currency/index'
-import * as nft from './nft/index'
-import * as marketplace from './marketplace/index'
-import * as services from './services/index'
-
-export { avatar, ethereum, mana, currency, nft, marketplace, services }
+export * as avatar from './avatar/index'
+export * as ethereum from './ethereum/index'
+export * as mana from './mana/index'
+export * as currency from './currency/index'
+export * as nft from './nft/index'
+export * as marketplace from './marketplace/index'
+export * as services from './services/index'
+export * as contract from './utils/contract'
