649dadb7878cb2949d2a00aafcb563757eb30d12
Nicolas Chamo <nicolas@chamo.com.ar>
2021-04-01 17:13:50 -0300

    First commit

--- src/wearable/index.ts
+++ src/wearable/index.ts
@@ -1,5 +1,36 @@
-import { Wearables } from "./types";
+import { getCatalystUrl, mapV2WearableIntoV1 } from "../shared/utils";
 
-export async function getListOfWearables(){
-  return await fetch('https://wearable-api.decentraland.org/v2/collections').then(res => res.json()) as Wearables[]
+export async function getListOfWearables(filters: AtLeastOne<WearableFilters>): Promise<Wearable[]> {
+  const queryParams = convertObjectToQueryParamString(filters)
+  const catalystUrl = await getCatalystUrl()
+
+  return fetch(`${catalystUrl}/lambdas/collections/wearables${queryParams}`)
+    .then(res => res.json())
+    .then(res => res.wearables)
+    .then(wearables => wearables.map(wearable => mapV2WearableIntoV1(catalystUrl, wearable)))
 }
+
+function convertObjectToQueryParamString(object: Record<string, number | boolean | string | number[] | boolean[] | string[]>):string {
+  let result = ""
+  for (const key in object) {
+    const value = object[key]
+    if (!value) continue
+    const name = key.substr(key.length - 1) === 's' ? key.slice(0, -1) : key
+    let values: string[]
+    if (Array.isArray(value)) {
+      values = [...value].map((_) => `${_}`)
+    } else {
+      values = [`${value}`]
+    }
+    result += result.length > 0 ? `?` : '&'
+    result += `${name}=` + values.join(`&${name}=`)
+  }
+  return result
+}
+
+type WearableFilters = {
+  collectionIds: string[]
+  wearableIds: string[]
+  textSearch: string
+}
+type AtLeastOne<T, U = {[K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
